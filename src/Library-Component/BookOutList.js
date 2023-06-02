17ff45185ddef564307bb7f9626a0d48da9294f1
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-06-02 22:34:36 +0530

    changes

---
+++ src/Library-Component/BookOutList.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {  booksContext } from '../App';
+import { useNavigate } from 'react-router-dom';
+
+export default function BookOutList (){
+  const navTo = useNavigate();
+
+  //delete functionality
+  const deleteOutBook = async (bookId) => {
+    const response = await fetch(
+      `https://6478876c362560649a2df68b.mockapi.io/students/${bookId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const data = await response.json();
+    if (data) {
+      const remainingbooks = outBooks.filter(
+        (stud, idx) => stud.id !== bookId
+      );
+      setOutBooks(remainingbooks);
+    }
+  };
+  //Getting books data
+  const {outBooks, setOutBooks} = useContext(booksContext);
+
+  return (
+    <div className="container w-100 ">
+      <div className="row justify-content-around p-3">
+        {outBooks.map((data, idx) => (
+          <Card
+            className="card col-sm-6 col-md-4 col-lg-3 border border-lightdark shadow rounded m-3 h-100 "
+            key={idx}
+          >
+            <Card.Body>
+              
+              <Card.Title>{data.name}</Card.Title>
+              <Card.Text>{data.roll_no}</Card.Text>
+              <Card.Text>{data.book_name}</Card.Text>
+              <Card.Text>{data.register_date}</Card.Text>
+              <Card.Text>{data.no_days_allowed}</Card.Text>
+              <Button className="mx-3 px-5" variant="success" onClick={() => deleteOutBook(data.id)}>
+                <i className="fa-solid fa-trash"></i>
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/NavBarPage.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBarPage = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-success px-3">
+        <Link class="navbar-brand text-light px-3" to="/home">
+          <h3>Dashboard</h3>
+        </Link>
+        <button
+          class="navbar-toggler border-0"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fa-solid fa-bars fa-xl text-light"/>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <Link className="btn text-light w-100 px-5" to="/BooksList">
+                Books page
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link className="btn text-light w-100 px-5" to="/outBooks">
+                Out books
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link className="btn text-light w-100 px-5" to="/addOutBooks">
+                Add Out books
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  )
+}
+
+export default NavBarPage
